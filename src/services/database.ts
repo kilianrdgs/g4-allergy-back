4e1e08beb201300744beb457192a927dc4d07087
kilianrdgs <kilian.rdgs@free.fr>
2024-02-07 11:26:16 +0100

    optimisation du fichier databse

--- src/services/database.ts
+++ src/services/database.ts
@@ -3,32 +3,10 @@ dotenv.config();
 const mongoose = require('mongoose');
 const URI: string = process.env.MONGO_URI || '';
 
-const User = require('../models/userModel');
-const Allergy = require('../models/allergyModel');
-
-async function main(){
-
-    mongoose.connect(URI)
-
-//    await utilisateur.save().then(() => console.log('utilisateur enregistré'));
-// await allergie.save().then(() => console.log('allergie enregistrée'));
+export default async function main(){
+    try {
+        mongoose.connect(URI)
+    } catch (error) {
+        console.error(`erreur lors de la  connexion : ${error}`);
+    }
 }
-
-const utilisateur = new User({
-    name: 'admin',
-    email: 'test@gmail.com',
-    password: 'azertyuiop',
-    isAdmin: false
-});
-
-const allergie = new Allergy({
-    name: 'test',
-    description: 'test',
-    createdBy: '0731263829312',
-    isPrivate: true
-});
-
-
-
-
-export default main;
