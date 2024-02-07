f954e2132ca5d25abc419711cbb02098149fe96d
Adam <adamboissy2004@gmail.com>
2024-02-07 11:09:47 +0100

    connexion fonctionnelle

--- app.ts
+++ app.ts
@@ -4,6 +4,12 @@ const port = 3001;
 import main from './src/services/database';
 import router from "./src/routes/userRoutes";
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 
 //Middlewares
 app.use(express.json());
