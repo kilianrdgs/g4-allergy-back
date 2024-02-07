421dfda2f05c8a6df194cee50a7982714f97db15
BASTIEN VITOUR <bastien.vitour@edu.esiee-it.fr>
2024-02-07 11:13:18 +0100

    enlevé commentaire

--- src/routes/allergyRoutes.ts
+++ src/routes/allergyRoutes.ts
@@ -5,18 +5,6 @@ import allergyController from '../controllers/allergyController'
 
 router.get('/list', async (req, res) => {
     const list = await allergyController.getAllergyList()
-    // const list = [
-    //     {name: "Bastien",
-    //     allergies: [
-    //     {name: "Pollen", isPrivate: true},
-    //     {name: "Cacahuètes", isPrivate: false}
-    //     ]},
-    //     {name: "Julien",
-    //     allergies: [
-    //     {name: "Gluten", isPrivate: true},
-    //     {name: "Connards", isPrivate: false}
-    //     ]}
-    // ]
     res.send(list)
 })
 
