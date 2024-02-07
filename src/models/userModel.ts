73c4dd34da68e6431e9bcb65361b6ce9fa3ed960
kilianrdgs <kilian.rdgs@free.fr>
2024-02-07 21:15:06 +0100

    rajout du .env salt

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken')
+const salt: number = process.env.SALT ? parseInt(process.env.SALT) : 10;
 
 const schema = new mongoose.Schema({
     name: {
@@ -34,12 +37,13 @@ schema.methods.generateAuthTokenAndSaveUser = async function(){
     const authToken = jwt.sign({ _id: this._id.toString()}, 'token');
     this.authTokens.push({authToken});
     await this.save();
+    console.log(salt)
     return authToken;
   }
 
 schema.pre("save", async function () {
     if (this.isModified("password")) {
-      this.password = await bcrypt.hash(this.password, 8);
+      this.password = await bcrypt.hash(this.password, salt);
     }
   });  
 
