a79eaf556f64e5231dce786b47db48b306577454
BASTIEN VITOUR <bastien.vitour@edu.esiee-it.fr>
2024-02-08 21:04:53 +0100

    suppression avec vérif admin

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -14,6 +14,12 @@ router.get('/isConnected', authentication , (req, res) => {
     res.json(true);
 });
 
+router.get('/user', authentication, (req, res) => {
+    if(req.body.user){
+        res.send({"name": req.body.user.name, "_id": req.body.user._id, "isAdmin": req.body.user.isAdmin})
+    }
+})
+
 //route pour se deconnecter
 router.post('/deconnexion', authentication , logoutUser);
 
