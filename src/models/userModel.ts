7553371fe0e9ae6ac3a4ae17b70cca634cc490ef
kilianrdgs <kilian.rdgs@free.fr>
2024-02-08 18:36:08 +0100

    securité nom et mdp

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,43 +1,65 @@
-import mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 dotenv.config();
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const salt: number = process.env.SALT ? parseInt(process.env.SALT) : 10;
-const secret_key: string = process.env.SECRET_KEY || '';
+const secret_key: string = process.env.SECRET_KEY || "";
 
 const schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    validateLength(v: string) {
+      if (v.length < 2) {
+        throw new Error("le nom ne doit pas être inférieur à 2 caractères");
+      }
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
+    validateMaxLength(v: string) {
+      if (v.length > 15) {
+        throw new Error("le nom ne doit pas être superieur à 15 caractères");
+      }
     },
-    password: {
-        type: String,
-        required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    validateLength(v: string) {
+      if (v.length < 6) {
+        throw new Error(
+          "le mot de passe ne doit pas être inférieur à 6 caractères"
+        );
+      }
     },
-    isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false
+    validate(v: string) {
+      if (!/\d/.test(v)) {
+        throw new Error("le mot de passe ne doit contenir au moins un chiffre");
+      }
     },
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
 });
 
-schema.methods.generateAuthTokenAndSaveUser = async function(){
-    const authToken = jwt.sign({ _id: this._id.toString()}, secret_key);
-    return authToken;
-  }
+schema.methods.generateAuthTokenAndSaveUser = async function () {
+  const authToken = jwt.sign({ _id: this._id.toString() }, secret_key);
+  return authToken;
+};
 
 schema.pre("save", async function () {
-    if (this.isModified("password")) {
-      this.password = await bcrypt.hash(this.password, salt);
-    }
-  });  
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+});
 
 const User = mongoose.model("users", schema, "users");
 
